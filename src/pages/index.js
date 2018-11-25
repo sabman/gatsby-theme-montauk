79de3121dfc4cbb07a1a94472c9e7d9c582a56a5
nodox <snatera09@gmail.com>
2018-11-24 19:29:17 -0500

    feat: replicate homepage mobile view

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,37 +2,156 @@ import "../styles/reboot.css"
 
 import React from 'react'
 import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 
-import styled, { keyframes } from "styled-components";
-import { CSSTransition } from 'react-transition-group';
+import styled, { createGlobalStyle } from "styled-components";
+import { Transition } from 'react-transition-group';
 
 
-
-const GMNav = styled.nav`
-  color: black;
-
-  max-height: ${props => props.isOpen ? "100%" : "0" };
-  overflow: ${props => props.isOpen ? "visible" : "hidden" };
-  visibility: ${props => props.isOpen ? "visible" : "hidden" };
-
-  ${'' /* transition: max-height,.3s; */}
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  #___gatsby,
+  #___gatsby > div {
+    width: 100%;
+    height: 100%;
+  }
 `
 
-const GMNavList = styled.ul`
-  padding-inline-start: 0;
-  margin-bottom: 0;
+const Grid = styled.div`
+  display: grid;
+  padding: 0 1rem;
 `
-const GMNavListItem = styled.li`
-  list-style-type: none;
+
+const Cell = styled.div`
+`
+
+const Navbar = styled.div`
+  display: grid;
+
+  padding: 1rem 0;
+
+  text-align: center;
+  text-transform: uppercase;
+  
+  border-bottom-style: solid;
+  border-bottom-color: rgba(0,0,0,.1);
+  border-bottom-width: 2px;
+`
+
+const NavbarItem = styled.div`
+  padding: 0.3rem 0;
+
+  color: rgba(0,0,0,.2);
+
+  text-transform: uppercase;
+
+  line-height: 1.6em;
+  letter-spacing: 1px;
+
+  font-size: 16px;
+  font-weight: 400;
+  font-style: normal;
+`
+
+const NavbarButton = styled.div`
+  padding: 15px 0;
+
+  color: rgba(0,0,0,.5);
+
+  text-align: center;
+  text-transform: uppercase;
+
+  line-height: 1.6em;
+  letter-spacing: 1px;
+
+  font-size: 16px;
+  font-weight: 400;
+  font-style: normal;
+
+  border-bottom-style: solid;
+  border-bottom-color: rgba(0,0,0,.1);
+  border-bottom-width: 2px;
+`
+
+const Header = styled.div`
+  padding: 30px 0;
   text-align: center;
 `
 
-const GMNavButton = styled.div`
-  text-align: center;
-  border: 1px solid black;
+const ListGroup = styled.div`
+  width: 100%;
 
+  padding: 15px 0;
+
+  color: rgba(0,0,0,.3);
+  line-height: 2em;
+  text-transform: uppercase;
+  text-decoration: none;
+  letter-spacing: 3px;
+
+  font-size: 14px;
+  font-weight: 600;
+  font-style: normal;
+
+  text-align: center;
 `
 
+const ListGroupItem = styled.div`
+`
+
+const Hero = styled.div`
+  position: relative;
+`
+
+const HeroTitle = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 0;
+  right: 0;
+  z-index: 1;
+
+  font-size: 21px;
+
+  color: white;
+  text-transform: uppercase;
+  text-align: center;
+`
+
+const Section = styled.div`
+  text-align: center;
+  margin: 40px 0;
+`
+
+const SectionBody = styled.p`
+  text-align: left;
+  font-size: 14px;
+  line-height: 1.8em;
+  text-transform: none;
+  text-decoration: none;
+  font-weight: 300;
+  font-style: normal;
+  margin: 0 0 1.8em;
+`
+
+const SectionHeader = styled.h3`
+  margin: 1em 0 .3em;
+  font-size: 1.1em;
+  line-height: 1.5em;
+  text-transform: none;
+  text-decoration: none;
+  letter-spacing: 1px;
+  font-weight: 400;
+  font-style: normal;
+`
+
+const Footer = styled.div`
+  padding: 50px 0;
+
+  border-top-style: solid;
+  border-top-color: rgba(0,0,0,.1);
+  border-top-width: 2px;
+`
 
 export default class IndexPage extends React.Component {
 
@@ -47,29 +166,95 @@ export default class IndexPage extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <GMNav isOpen={this.state.open}>
-          <GMNavList>
-            <GMNavListItem>
-              <Link to="#">artists</Link>
-            </GMNavListItem>
-            <GMNavListItem>
-              <Link to="#">blog</Link>
-            </GMNavListItem>
-            <GMNavListItem>
-              <Link to="#">contact</Link>
-            </GMNavListItem>
-            <GMNavListItem>
-              <Link to="#">shop</Link>
-            </GMNavListItem>
-            <GMNavListItem>
-              <Link to="#">about</Link>
-            </GMNavListItem>
-          </GMNavList>
-        </GMNav>
-        <GMNavButton onClick={this.handleMenuClick}>
-          <button>menu</button>
-        </GMNavButton>
+        <Grid>
+          <Cell>
+            <Navbar>
+              <NavbarItem>artists</NavbarItem>
+              <NavbarItem>blog</NavbarItem>
+              <NavbarItem>contact</NavbarItem>
+              <NavbarItem>shop</NavbarItem>
+              <NavbarItem>about</NavbarItem>
+            </Navbar>
+            <NavbarButton>menu</NavbarButton>
+          </Cell>
+          <Cell>
+            <Header>
+              <Img fixed={this.props.data.logo.fixed} />
+              <ListGroup>
+                <ListGroupItem>51 edgemere street</ListGroupItem>
+                <ListGroupItem>montauk, ny, 11954</ListGroupItem>
+                <ListGroupItem>(800) 123-4567</ListGroupItem>
+              </ListGroup>
+            </Header>
+          </Cell>
+          <Cell>
+            <Hero>
+              <HeroTitle>welcome</HeroTitle>
+              <Img fluid={this.props.data.wilderness.fluid} />
+            </Hero>
+            <Section>
+              <SectionBody>
+                Vestibulum id ligula porta felis euismod semper. 
+                Etiam porta sem malesuada magna mollis euismod. 
+                Nulla vitae elit libero, a pharetra augue. Nullam id dolor 
+                id nibh ultricies vehicula ut id elit. Praesent commodo cursus 
+                magna, vel scelerisque nisl consectetur et. Sed posuere consectetur 
+                est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. 
+                Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean eu leo 
+                quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. 
+                Maecenas sed diam eget risus varius blandit sit
+                amet non magna. Cras mattis consectetur purus sit amet fermentum.
+              </SectionBody>
+              <SectionHeader>
+                <Link to="/" style={{ color: "black" }}>
+                  View Artists &nbsp;→
+                </Link>
+              </SectionHeader>
+            </Section>
+          </Cell>
+          <Cell>
+            <Footer>
+                Made by Gatsby Manor
+            </Footer>
+          </Cell>
+        </Grid>
       </React.Fragment>
     )
   }
 }
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    logo: imageSharp(fluid: {  originalName: { regex: "/mlogo/"} }) {
+      fixed(height: 100, width: 120) {
+        base64
+        tracedSVG
+        aspectRatio
+        width
+        height
+        src
+        srcSet
+        srcWebp
+        srcSetWebp
+        originalName
+      }
+    }
+
+    wilderness: imageSharp(fluid: {  originalName: { regex: "/wilderness/"} }) {
+      fluid {
+        base64
+        tracedSVG
+        aspectRatio
+        sizes
+        src
+        srcSet
+        srcWebp
+        srcSetWebp
+        originalName
+      }
+    }
+
+  }
+`
+
+
